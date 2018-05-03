8c71a7bae5c83a3e1496c673d7766066b07c4140
asJinbinZhu <jinbinzhu@outlook.com>
2018-05-03 22:13:40 +0800

    test

--- _003LSTM_for_MNIST/_000DataLoader.py
+++ _003LSTM_for_MNIST/_000DataLoader.py
@@ -16,7 +16,7 @@ torch_dataset = Data.TensorDataset(data_tensor=x, target_tensor=y)
 data_loader = Data.DataLoader(
     dataset=torch_dataset,
     batch_size=batch_size,
-    shuffle=False,
+    shuffle=True,
     num_workers=1
 )
 
@@ -52,4 +52,68 @@ y:
   9
  10
 [torch.FloatTensor of size 10]
+
+# shuffle = False
+print('Step: ', step, 'batch_x: ', batch_x, 'batch_y: ', batch_y)
+Step:  0 batch_x:  
+ 1
+ 2
+ 3
+ 4
+ 5
+[torch.DoubleTensor of size 5]
+ batch_y:  
+ 1
+ 2
+ 3
+ 4
+ 5
+[torch.DoubleTensor of size 5]
+
+Step:  1 batch_x:  
+  6
+  7
+  8
+  9
+ 10
+[torch.DoubleTensor of size 5]
+ batch_y:  
+  6
+  7
+  8
+  9
+ 10
+[torch.DoubleTensor of size 5]
+
+# shuffle = True
+print('Step: ', step, 'batch_x: ', batch_x, 'batch_y: ', batch_y)
+Step:  0 batch_x:  
+  5
+  7
+ 10
+  3
+  4
+[torch.DoubleTensor of size 5]
+ batch_y:  
+  5
+  7
+ 10
+  3
+  4
+[torch.DoubleTensor of size 5]
+
+Step:  1 batch_x:  
+ 2
+ 1
+ 8
+ 9
+ 6
+[torch.DoubleTensor of size 5]
+ batch_y:  
+ 2
+ 1
+ 8
+ 9
+ 6
+[torch.DoubleTensor of size 5]
 '''
